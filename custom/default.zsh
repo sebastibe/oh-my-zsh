d7a19c9952385fcc852bd3ea7345f956a04d9059
Sebastien Beal <sebastibe@gmail.com>
2013-04-07 15:45:57 +0900

    Track your current directory in emacs as you cd around

--- custom/default.zsh
+++ custom/default.zsh
@@ -4,3 +4,10 @@
 alias pm="python manage.py"
 alias pmt="python manage.py test"
 alias pmrs="python manage.py runserver"
+
+# track your current directory in emacs as you cd around
+if [ -n "$INSIDE_EMACS" ]; then
+  chpwd() { print -P "\033AnSiTc %d" }
+  print -P "\033AnSiTu %n"
+  print -P "\033AnSiTc %d"
+fi
