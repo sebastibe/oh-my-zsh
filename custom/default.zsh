76afe51d7a49107c66ccffa47934dd2f84e19b25
Sebastien Beal <sebastibe@gmail.com>
2013-04-23 14:01:35 +0200

    Add nocorrect for 'fab'

--- custom/default.zsh
+++ custom/default.zsh
@@ -5,6 +5,12 @@ alias pm="python manage.py"
 alias pmt="python manage.py test"
 alias pmrs="python manage.py runserver"
 
+# nocorrect
+alias fab="nocorrect fab"
+
+# disable autocorrect
+# unsetopt correct
+
 # track your current directory in emacs as you cd around
 if [ -n "$INSIDE_EMACS" ]; then
   chpwd() { print -P "\033AnSiTc %d" }
